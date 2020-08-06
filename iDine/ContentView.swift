0c7199fce7987f3093724162db2e853e7c0ae222
Anurak Teerarattananukulchai <anurak.teera@gmail.com>
2020-08-06 15:37:13 +0700

    Initial Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by anurak teerarattananukulchai on 6/8/2563 BE.
+//  Copyright © 2563 anurak teerarattananukulchai. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
